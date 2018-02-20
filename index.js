822475d7c0045c2f40e40c9a267cd2fa073d4408
Brent Bauman <brentbauman86@gmail.com>
2018-02-20 20:47:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,8 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, dinner, meat){
 function deleteFromObjectByKey(recipes, dinner, meat){
   return Object.assign({}, recipes, { [dinner]: meat })
 };
+
+function destructivelyDeleteFromObjectByKey(recipes, dinner, meat){
+  recipes[dinner] = meat
+  return recipes
+};
