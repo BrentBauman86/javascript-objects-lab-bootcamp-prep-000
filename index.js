17fcc5a9e581070a333b7947912f7246b3133311
Brent Bauman <brentbauman86@gmail.com>
2018-02-20 20:34:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(recipes, dinner, meat){
   recipes[dinner] = meat
   return recipes
 };
+
+function updateObjectWithKeyAndValue(recipes, dinner, meat){
+  recipes[dinner] = meat
+  return Object.assign({}, recipes, { [dinner]: meat })
+};
